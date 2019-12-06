b2fb77bf0d8aa896057758527e72489e675ffd4e
moncho <50428+moncho@users.noreply.github.com>
2019-12-06 20:33:37 +0100

    solve day5 part1

---
+++ day5/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"aoc-2019/intcode"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var program []int
+	for scanner.Scan() {
+		t := scanner.Text()
+		for _, s := range strings.Split(t, ",") {
+			i, err := strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+			program = append(program, i)
+		}
+	}
+	computer := intcode.New(program)
+	out, err := computer.Run(1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce? %d\n", out)
+
+}
