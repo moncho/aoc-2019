0b84a176c7b7b11341c70d748e179a69b00a5b79
moncho <50428+moncho@users.noreply.github.com>
2019-12-08 10:08:08 +0100

    solve day6 part2

--- day6/main.go
+++ day6/main.go
@@ -32,4 +32,30 @@ func main() {
 		}
 	}
 	fmt.Printf("What is the total number of direct and indirect orbits in your map data? %d\n", count)
+
+	myPath := orbitalPath(orbits, "YOU")
+	santaPath := orbitalPath(orbits, "SAN")
+
+	transfers := 0
+
+found:
+	for i, me := range myPath {
+		for j, santa := range santaPath {
+			if santa == me {
+				transfers = i + j
+				break found
+			}
+		}
+
+	}
+
+	fmt.Printf("What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting? %d\n", transfers)
+}
+
+func orbitalPath(orbits map[string]string, s string) []string {
+	var path []string
+	for k, ok := orbits[s]; ok; k, ok = orbits[k] {
+		path = append(path, k)
+	}
+	return path
 }
