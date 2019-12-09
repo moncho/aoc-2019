63ecd0997adfb9e1ae60c3b76549b01a9c4ac41c
moncho <50428+moncho@users.noreply.github.com>
2019-12-09 21:58:45 +0100

    solve day7 part2

--- day7/main.go
+++ day7/main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-type amplifiers []intcode.Computer
+type amplifiers []*intcode.V2Computer
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -32,10 +32,10 @@ func main() {
 			program = append(program, i)
 		}
 	}
-	phases := phases(5)
+
 	var maxOutput int
 	var maxPhase []int
-	for _, phase := range phases {
+	for _, phase := range possiblePhases(5, 0) {
 		amplifiers := new(program)
 		output := amplifiers.output(phase)
 		if output > maxOutput {
@@ -46,35 +46,72 @@ func main() {
 
 	fmt.Printf("What is the highest signal that can be sent to the thrusters? %d. Phase: %v\n", maxOutput, maxPhase)
 
+	for _, phase := range possiblePhases(5, 5) {
+		amplifiers := new(program)
+		output := amplifiers.feedbackLoop(phase)
+		if output > maxOutput {
+			maxOutput = output
+			maxPhase = phase
+		}
+	}
+
+	fmt.Printf("What is the highest signal that can be sent to the thrusters? %d. Phase: %v\n", maxOutput, maxPhase)
+
 }
-func phases(amplifiers int) [][]int {
+func possiblePhases(amplifiers int, basePower int) [][]int {
 	base := make([]int, amplifiers)
 
 	for i := 0; i < amplifiers; i++ {
-		base[i] = i
+		base[i] = i + basePower
 	}
 	return permutations(base)
 }
 func new(program []int) amplifiers {
-	return []intcode.Computer{
-		intcode.New(program),
-		intcode.New(program),
-		intcode.New(program),
-		intcode.New(program),
-		intcode.New(program),
+	return []*intcode.V2Computer{
+		intcode.NewV2(program),
+		intcode.NewV2(program),
+		intcode.NewV2(program),
+		intcode.NewV2(program),
+		intcode.NewV2(program),
 	}
 }
 
 func (a amplifiers) output(phase []int) int {
 	var output int
 	for i, ampli := range a {
-		out, err := ampli.Run(phase[i], output)
-		if err != nil {
-			panic(err)
-		}
-		output = out
+		ampli.Run()
+		ampli.In(phase[i])
+		ampli.In(output)
+		output = ampli.Out()
 	}
 	return output
+}
+
+func (a amplifiers) feedbackLoop(phase []int) int {
+
+	// done channel for the last amplifier
+	var done <-chan bool
+	for i, ampli := range a {
+		done = ampli.Run()
+		ampli.In(phase[i])
+	}
+	i := 0
+	next := 0
+	for {
+		select {
+		case <-done:
+			return next
+		default:
+		}
+
+		a[i].In(next)
+		next = a[i].Out()
+		if i == len(a)-1 {
+			i = 0
+		} else {
+			i++
+		}
+	}
 }
 
 func factorial(n int) int {
