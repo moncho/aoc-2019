b228f8ed7961f1e8c77436fd4970b3e090c6494d
moncho <50428+moncho@users.noreply.github.com>
2019-12-29 11:46:29 +0100

    solve day 22 part2

--- day22/main.go
+++ day22/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math/big"
 	"os"
 	"strconv"
 	"strings"
@@ -21,20 +22,32 @@ func main() {
 
 	defer file.Close()
 
-	instructions := shuffleInstructions(file)
-	var deck deck = make([]int, 10007)
-	for i := 0; i < len(deck); i++ {
-		deck[i] = i
-	}
-
-	deck.shuffle(instructions...)
+	instructions := instructions(file)
+	deck := newDeck(10007)
+	deck.shuffle(instructions)
 
 	fmt.Printf("After shuffling your factory order deck of 10007 cards, what is the position of card 2019? %d\n", deck.position(2019))
+
+	cards := new(big.Int)
+	cards.SetString("119315717514047", 10)
+
+	iterations := new(big.Int)
+	iterations.SetString("101741582076661", 10)
+	card := cardInPos(cards, iterations, big.NewInt(2020), instructions)
+	fmt.Printf("After shuffling your new, giant, factory order deck that many times, what number is on the card that ends up in position 2020? %d\n", card)
+
 }
 
-func (d deck) shuffle(tt ...technique) {
-	for _, t := range tt {
-		t(d)
+func newDeck(cards int) deck {
+	deck := make([]int, cards)
+	for i := 0; i < cards; i++ {
+		deck[i] = i
+	}
+	return deck
+}
+func (d deck) shuffle(instructions []string) {
+	for _, instruction := range instructions {
+		parseTechnique(instruction)(d)
 	}
 }
 func (d deck) position(card int) int {
@@ -45,17 +58,16 @@ func (d deck) position(card int) int {
 	}
 	return -1
 }
-func shuffleInstructions(r io.Reader) []technique {
-	var tt []technique
+func instructions(r io.Reader) []string {
+	var tt []string
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		tt = append(tt, newTechnique(scanner.Text()))
+		tt = append(tt, strings.TrimSpace(scanner.Text()))
 	}
 	return tt
 }
-func newTechnique(s string) technique {
+func parseTechnique(s string) technique {
 
-	s = strings.TrimSpace(s)
 	if strings.HasPrefix(s, "cut") {
 		i, err := strconv.Atoi(s[len("cut "):])
 		if err != nil {
@@ -87,6 +99,7 @@ func cut(i int) technique {
 		copy(a, cutted)
 	}
 }
+
 func deal(i int) technique {
 	return func(a []int) {
 		temp := make([]int, len(a))
@@ -108,4 +121,116 @@ func dealNewStack() technique {
 			a[i], a[opp] = a[opp], a[i]
 		}
 	}
+}
+
+func parse(s string) technique {
+
+	s = strings.TrimSpace(s)
+	if strings.HasPrefix(s, "cut") {
+		i, err := strconv.Atoi(s[len("cut "):])
+		if err != nil {
+			panic(err)
+		}
+		return cut(i)
+	} else if strings.HasPrefix(s, "deal with increment") {
+		i, err := strconv.Atoi(s[len("deal with increment "):])
+		if err != nil {
+			panic(err)
+		}
+		return deal(i)
+	} else if strings.HasPrefix(s, "deal into new stack") {
+		return dealNewStack()
+	} else {
+		panic(s)
+	}
+
+}
+
+func cardInPos(cards, iterations, cardPos *big.Int, rules []string) *big.Int {
+	// Build a linear formula to calculate a new card position from an initial position
+	// and a set of shuffling techniques.
+	// New cards positions is a*cardPos+b
+	// Calculate a and b
+	a, b := suffleFormula(cards, rules)
+	//run "ax+b" iterations times
+	a, b = modpow(a, b, iterations, cards)
+	// New cards positions is a*cardPos+b%cards
+	a = a.Mul(a, cardPos)
+	a = a.Add(a, b)
+	return a.Mod(a, cards)
+}
+
+// Defines a formula ax+b from the shuffling techniques. Techniques are read from last to
+// first and, since it goes backward, the reversal of each technique has to be used.
+// Inspired by https://www.reddit.com/r/adventofcode/comments/eeeixy/remember_the_challenges_arent_here_for_you_to/
+func suffleFormula(l *big.Int, r []string) (*big.Int, *big.Int) {
+	a := big.NewInt(1)
+	b := big.NewInt(0)
+	for i := len(r) - 1; i >= 0; i-- {
+		s := strings.TrimSpace(r[i])
+		if strings.HasPrefix(s, "cut") {
+			n, ok := new(big.Int).SetString(s[len("cut "):], 10)
+			if !ok {
+				panic(s)
+			}
+			b = b.Add(b, n)
+			b = b.Mod(b, l)
+		} else if strings.HasPrefix(s, "deal with increment") {
+			n, ok := new(big.Int).SetString(s[len("deal with increment "):], 10)
+			if !ok {
+				panic(s)
+			}
+
+			z := new(big.Int)
+			z = z.ModInverse(n, l)
+
+			a = a.Mul(a, z)
+			a = a.Mod(a, l)
+
+			b = b.Mul(b, z)
+			b = b.Mod(b, l)
+
+		} else if strings.HasPrefix(s, "deal into new stack") {
+			a = a.Neg(a)
+			b.Sub(l, b)
+			b.Sub(b, big.NewInt(1))
+		} else {
+			panic(s)
+		}
+	}
+	return a, b
+}
+
+// modpow the polynomial: (ax+b)^iterations % cards
+func modpow(a, b, iterations, cards *big.Int) (*big.Int, *big.Int) {
+
+	if iterations.Int64() == 0 {
+		return big.NewInt(1), big.NewInt(0)
+	}
+	if iterations.Int64()%2 == 0 {
+		abb := new(big.Int)
+		abb = abb.Mul(a, b)
+		abb = abb.Add(abb, b)
+		abb = abb.Mod(abb, cards)
+
+		aExp := new(big.Int)
+		aExp = aExp.Exp(a, big.NewInt(2), cards)
+		md := new(big.Int)
+		md = md.Div(iterations, big.NewInt(2))
+		return modpow(aExp, abb, md, cards)
+	}
+
+	newM := new(big.Int)
+	newM = newM.Sub(iterations, big.NewInt(1))
+
+	c, d := modpow(a, b, newM, cards)
+	ac := new(big.Int)
+	ac = ac.Mul(a, c)
+	ac = ac.Mod(ac, cards)
+
+	ad := new(big.Int)
+	ad = ad.Mul(a, d)
+	ad = ad.Add(ad, b)
+	ad = ad.Mod(ad, cards)
+	return ac, ad
 }
--- day22/main_test.go
+++ day22/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math/big"
+	"os"
 	"reflect"
 	"strings"
 	"testing"
@@ -64,8 +66,8 @@ func Test_deck_shuffle(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			instructions := shuffleInstructions(strings.NewReader(tt.args.s))
-			tt.d.shuffle(instructions...)
+			instructions := instructions(strings.NewReader(tt.args.s))
+			tt.d.shuffle(instructions)
 
 			if !reflect.DeepEqual(tt.d, tt.expect) {
 				t.Errorf("After shuffling, expected: %v, got: %v", tt.expect, tt.d)
@@ -85,7 +87,7 @@ func Test_deal(t *testing.T) {
 		want deck
 	}{
 		{
-			"",
+			"deal with increment 3",
 			args{
 				3,
 			},
@@ -93,7 +95,7 @@ func Test_deal(t *testing.T) {
 			[]int{0, 7, 4, 1, 8, 5, 2, 9, 6, 3},
 		},
 		{
-			"",
+			"deal with increment 7",
 			args{
 				7,
 			},
@@ -103,8 +105,7 @@ func Test_deal(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			d := deal(tt.args.i)
-			tt.deck.shuffle(d)
+			tt.deck.shuffle([]string{tt.name})
 			if !reflect.DeepEqual(tt.deck, tt.want) {
 				t.Errorf("deal() = %v, want %v", tt.deck, tt.want)
 			}
@@ -123,7 +124,7 @@ func Test_cut(t *testing.T) {
 		want deck
 	}{
 		{
-			"",
+			"cut 3",
 			args{
 				3,
 			},
@@ -131,15 +132,15 @@ func Test_cut(t *testing.T) {
 			[]int{3, 4, 5, 6, 7, 8, 9, 0, 1, 2},
 		},
 		{
-			"",
+			"cut 2",
 			args{
 				2,
 			},
 			[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-			[]int{3, 4, 5, 6, 7, 8, 9, 0, 1, 2},
+			[]int{2, 3, 4, 5, 6, 7, 8, 9, 0, 1},
 		},
 		{
-			"",
+			"cut -4",
 			args{
 				-4,
 			},
@@ -147,7 +148,7 @@ func Test_cut(t *testing.T) {
 			[]int{6, 7, 8, 9, 0, 1, 2, 3, 4, 5},
 		},
 		{
-			"",
+			"cut -2",
 			args{
 				-2,
 			},
@@ -157,10 +158,9 @@ func Test_cut(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			d := cut(tt.args.i)
-			tt.deck.shuffle(d)
+			tt.deck.shuffle([]string{tt.name})
 			if !reflect.DeepEqual(tt.deck, tt.want) {
-				t.Errorf("deal() = %v, want %v", tt.deck, tt.want)
+				t.Errorf("cut() = %v, want %v", tt.deck, tt.want)
 			}
 		})
 	}
@@ -173,17 +173,56 @@ func Test_dealNewStack(t *testing.T) {
 		want deck
 	}{
 		{
-			"",
+			"deal into new stack",
 			[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 			[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			d := dealNewStack()
-			tt.deck.shuffle(d)
+			tt.deck.shuffle([]string{tt.name})
 			if !reflect.DeepEqual(tt.deck, tt.want) {
 				t.Errorf("deal() = %v, want %v", tt.deck, tt.want)
+			}
+		})
+	}
+}
+
+func Test_cardInPos(t *testing.T) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	defer file.Close()
+
+	instructions := instructions(file)
+	type args struct {
+		cards      *big.Int
+		iterations *big.Int
+		cardPos    *big.Int
+		rules      []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *big.Int
+	}{
+		{
+			"10007 cards, 1 iteration",
+			args{
+				big.NewInt(10007),
+				big.NewInt(1),
+				big.NewInt(3324),
+				instructions,
+			},
+			big.NewInt(2019),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cardInPos(tt.args.cards, tt.args.iterations, tt.args.cardPos, tt.args.rules); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("cardInPos() = %v, want %v", got, tt.want)
 			}
 		})
 	}
